d839e5f39c539ad25e0ac736bb5c4145039f2e5a
sujai257 <115451876+sujai257@users.noreply.github.com>
2023-07-11 09:13:25 +0530

    app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ app =Flask(__name__)
 app.config["MYSQL_HOST"]="localhost"
 app.config["MYSQL_USER"]="root"
 app.config["MYSQL_PASSWORD"]="root"
-app.config["MYSQL_DB"]="sujai"
+app.config["MYSQL_DB"]="dbname"
 app.config["MYSQL_CURSORCLASS"]="DictCursor"
 mysql=MySQL(app)
 
