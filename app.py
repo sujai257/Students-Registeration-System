c0c9afa5078d08176d6001c6ff2e65ee0e3ca44c
sujai257 <sujaisujai257@gmail.com>
2023-07-30 18:54:26 +0530

    UI change

--- app.py
+++ app.py
@@ -1,108 +1,120 @@
-from flask import Flask,render_template,redirect,url_for
+from flask import Flask, render_template, redirect, url_for, request
 from flask_mysqldb import MySQL
-from flask.templating import render_template_string
-from flask.globals import request
-from pip._internal import req
-from test.test_contains import seq
-from flask_mail import Mail,Message
-app =Flask(__name__)
-app.config["MYSQL_HOST"]="localhost"
-app.config["MYSQL_USER"]="root"
-app.config["MYSQL_PASSWORD"]="root"
-app.config["MYSQL_DB"]="dbname"
-app.config["MYSQL_CURSORCLASS"]="DictCursor"
-mysql=MySQL(app)
+from flask_mail import Mail, Message
 
-mail = Mail(app) 
-app.config['MAIL_SERVER']='smtp.gmail.com'
+app = Flask(__name__)
+
+# MySQL Configuration
+app.config["MYSQL_HOST"] = "localhost"
+app.config["MYSQL_USER"] = "root"
+app.config["MYSQL_PASSWORD"] = "root"
+app.config["MYSQL_DB"] = "sujai"
+app.config["MYSQL_CURSORCLASS"] = "DictCursor"
+mysql = MySQL(app)
+
+# Mail Configuration
+app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 465
-app.config['MAIL_USERNAME'] = 'mailid@gmail.com'
-app.config['MAIL_PASSWORD'] = '******'
+app.config['MAIL_USERNAME'] = '20s147@kce.ac.in'  # Replace with your email
+app.config['MAIL_PASSWORD'] = 'Sujai@257'  # Replace with your email password
 app.config['MAIL_USE_TLS'] = False
 app.config['MAIL_USE_SSL'] = True
 mail = Mail(app)
+
+
 @app.route('/')
 def index():
-    con = mysql.connection.cursor()
-    sql ='select * from student'
-    con.execute(sql)
-    res=con.fetchall()
-    success_message = request.args.get('success_message')
-    if success_message:
-        return render_template('index.html', data=res, success_message=success_message)
-    else:
-        return render_template('index.html', data=res)   
+    try:
+        con = mysql.connection.cursor()
+        sql = 'select * from student'
+        con.execute(sql)
+        res = con.fetchall()
+        con.close()
+        success_message = request.args.get('success_message')
+        if success_message:
+            return render_template('index.html', data=res, success_message=success_message)
+        else:
+            return render_template('index.html', data=res)
+    except Exception as e:
+        return render_template("error.html", error_message=str(e))
 
-@app.route('/addrecord',methods=['GET','POST'])
+
+@app.route('/addrecord', methods=['GET', 'POST'])
 def addrecord():
-    if(request.method=='POST'):
-        roll=request.form['roll']
-        name=request.form['name']
-        age=request.form['age']
-        gender=request.form['gender']
-        email=request.form['email']
-        dept=request.form['department']
-        year=request.form['year']
-        ph=request.form['ph']
-        con=mysql.connection.cursor()
-        try:      
-            sql="insert into student values(%s,%s,%s,%s,%s,%s,%s,%s);"
-            con.execute(sql,[roll,name,age,gender,email,dept,year,ph])
+    try:
+        if request.method == 'POST':
+            roll = request.form['roll']
+            name = request.form['name']
+            age = request.form['age']
+            gender = request.form['gender']
+            email = request.form['email']
+            dept = request.form['department']
+            year = request.form['year']
+            ph = request.form['ph']
+            con = mysql.connection.cursor()
+            sql = "INSERT INTO student VALUES (%s, %s, %s, %s, %s, %s, %s, %s);"
+            con.execute(sql, [roll, name, age, gender, email, dept, year, ph])
             mysql.connection.commit()
-        except:
-            return render_template("error.html")
-        con.close()
-        return redirect(url_for('index'))
-    return render_template('addrecord.html')
+            con.close()
+            return redirect(url_for('index'))
+        return render_template('addrecord.html')
+    except Exception as e:
+        return render_template("error.html", error_message=str(e))
 
-@app.route("/edit/<string:roll>",methods=['GET','POST'])
+
+@app.route("/edit/<string:roll>", methods=['GET', 'POST'])
 def edit(roll):
-    con=mysql.connection.cursor()
-    if request.method=='POST':
-        name=request.form['name']
-        age=request.form['age']
-        gender=request.form['gender']
-        email=request.form['email']
-        dept=request.form['department']
-        year=request.form['year']
-        ph=request.form['ph']
-        upq='update student  set name=%s,age=%s,gender=%s,email_id=%s,dept=%s,year=%s,ph_no=%s where roll_no=%s'
-        con.execute(upq,[name,age,gender,email,dept,year,ph,roll])
-        mysql.connection.commit()
+    try:
+        con = mysql.connection.cursor()
+        if request.method == 'POST':
+            name = request.form['name']
+            age = request.form['age']
+            gender = request.form['gender']
+            email = request.form['email']
+            dept = request.form['department']
+            year = request.form['year']
+            ph = request.form['ph']
+            upq = 'UPDATE student SET name=%s, age=%s, gender=%s, email_id=%s, dept=%s, year=%s, ph_no=%s WHERE roll_no=%s'
+            con.execute(upq, [name, age, gender, email, dept, year, ph, roll])
+            mysql.connection.commit()
+            con.close()
+            return redirect(url_for('index'))
+        sql = "SELECT * FROM student WHERE roll_no=%s"
+        con.execute(sql, [roll])
+        res = con.fetchone()
         con.close()
-        return redirect(url_for('index'))
-    con=mysql.connection.cursor()
-    sql="select * from student where roll_no=%s"
-    con.execute(sql,[roll])
-    res=con.fetchone()    
-    return render_template("edit.html",data =res)
+        return render_template("edit.html", data=res)
+    except Exception as e:
+        return render_template("error.html", error_message=str(e))
+
 
 @app.route('/remove/<string:roll>')
 def remove(roll):
-    con = mysql.connection.cursor()
-    sql = 'DELETE FROM student WHERE roll_no = %s'
-    con.execute(sql, [roll])
-    mysql.connection.commit()
-    con.close()
-    success_message = "Record deleted successfully"
-    return redirect(url_for('index', success_message=success_message))
+    try:
+        con = mysql.connection.cursor()
+        sql = 'DELETE FROM student WHERE roll_no = %s'
+        con.execute(sql, [roll])
+        mysql.connection.commit()
+        con.close()
+        success_message = "Record deleted successfully"
+        return redirect(url_for('index', success_message=success_message))
+    except Exception as e:
+        return render_template("error.html", error_message=str(e))
 
-@app.route('/mailedit/<string:mm>',methods=['POST','GET'])
+
+@app.route('/mailedit/<string:mm>', methods=['POST', 'GET'])
 def mailedit(mm):
-    if request.method=='POST':
-        try:       
-            rev= request.form ['email'] 
-            msg = Message(request.form['subject'],sender ='mailid@gmail.com',recipients = [rev])
+    try:
+        if request.method == 'POST':
+            rev = request.form['email']
+            msg = Message(request.form['subject'], sender='20s147@kce.ac.in', recipients=[rev])
             msg.body = request.form['message']
             mail.send(msg)
-        except:
-            return render_template('error.html')
-        else:
-            return redirect(url_for('index'))       
-    return render_template("sendmail.html",data =mm)   
-print("Hello")
-if __name__ == ("__main__"):
-    app.run(debug=True) 
+            return redirect(url_for('index'))
+        return render_template("sendmail.html", data=mm)
+    except Exception as e:
+        return render_template("error.html", error_message=str(e))
 
 
-     
+if __name__ == "__main__":
+    app.run(debug=True)
